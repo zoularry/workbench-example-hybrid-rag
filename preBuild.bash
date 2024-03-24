e010899726a285bdea697343776df2fdc3ab80d3
zoularry <larry.zou@gmail.com>
2024-03-24 14:37:46 +0800

    Update preBuild.bash
    add tsinghua source for pip/apt

--- preBuild.bash
+++ preBuild.bash
@@ -4,6 +4,22 @@
 #
 # Note: This file may be removed if you don't need to use it
 export DEBIAN_FRONTEND=noninteractive
+
+# add pip tsinghua source
+sudo -E mkdir -p /root/.pip && \
+    echo "[global]" >> /root/.pip/pip.conf && \
+    echo "index-url = https://pypi.tuna.tsinghua.edu.cn/simple" >> /root/.pip/pip.conf
+
+# Update the package index and install necessary packages
+sudo -E apt-get update && apt-get install -y \
+    software-properties-common \
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
+# Add the Tsinghua University repository
+sudo -E add-apt-repository "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $(lsb_release -sc) main restricted universe multiverse"
+
+# Add the Tsinghua University repository key
+sudo -E apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+
 sudo -E apt-get update
 sudo -E apt-get install -y --no-install-recommends tzdata
 sudo -E ln -fs /usr/share/zoneinfo/Etc/UTC /etc/localtime
